b45a58fc8a5a64d7582b69eb4425672071154db5
Guilherme Varussa <guilherme.varussa@controlsoft.com.br>
2023-08-10 20:58:45 -0400

    created all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { SvgComponentsModule } from './modules/svg-components.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardBoxComponent } from './components/card-box/card-box.component';
+import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
+import { AmountBoxComponent } from './components/amount-box/amount-box.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		HeaderComponent,
+		CardBoxComponent,
+		CarouselItemsComponent,
+		AmountBoxComponent,
+		HomeComponent,
 	],
 	imports: [
 		BrowserModule,
