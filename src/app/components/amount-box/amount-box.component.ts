ab7189335e2c91b4df71aa41fed6f772ef2ab048
Guilherme Varussa <guilherme.varussa@controlsoft.com.br>
2023-08-10 22:11:02 -0400

    added amount service and integrate

--- src/app/components/amount-box/amount-box.component.ts
+++ src/app/components/amount-box/amount-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AmountDataModel } from 'src/app/model/amountDataModel';
 import { AccountsService } from 'src/app/services/accounts.service';
 
@@ -7,14 +7,36 @@ import { AccountsService } from 'src/app/services/accounts.service';
   templateUrl: './amount-box.component.html',
   styleUrls: ['./amount-box.component.css']
 })
-export class AmountBoxComponent {
+export class AmountBoxComponent implements OnInit {
 
   constructor(private amounnt: AccountsService) { }
+
 
   accountDetails: AmountDataModel = {
     amountValue: 0,
     limit: 0,
     totalAmount: 0
+  }
+
+
+
+
+  ngOnInit(): void {
+
+    this.getAmountValues()
+
+  }
+
+
+
+  getAmountValues() {
+    this.amounnt.getAccount().subscribe(data => {
+
+      this.accountDetails.amountValue = data.account.balance
+      this.accountDetails.limit = data.account.limit
+      this.accountDetails.totalAmount = this.accountDetails.amountValue + this.accountDetails.limit
+
+    })
   }
 
 }
--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { enviroment } from '../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
+  private apiUrl = enviroment.API_URL
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getAccount(): Observable<any> {
+    return this.http.get<any>(this.apiUrl)
+  }
+
+
 }
